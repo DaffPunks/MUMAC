f72fa2b93c513b0ef77c4b08a266ed09d14d0ac4
Vlad <vldvel@rambler.ru>
2017-05-30 14:22:10 +0300

    (u) up mumac-page

--- page-mumac.php
+++ page-mumac.php
@@ -20,45 +20,71 @@ add_action('wp_enqueue_scripts', 'gwc_enqueue_styles');
 get_header();
 ?>
 
-<div class="container-fluid">
+<div class="mumac-page">
 
-    <div class="page-top full-height-screen__img mumac-top__img">
-        <h1 class="page-top__header">Mumac</h1>
-        <p class="page-top__description">Lorem ipsum dolor sit amet, consectetur adipisicing.
-        elit sed do. eiusmod tempor incididunt ut labore et</p>
-        <a href="#" class="page-top__link">Scopri</a>
+    <div class="page-top">
+        <div class="page-top__text-block">
+            <h1 class="page-top__header">Mumac</h1>
+            <p class="page-top__description">Lorem ipsum dolor sit amet, consectetur adipisicing.
+            elit sed do. eiusmod tempor incididunt ut labore et</p>
+            <a href="#" class="page-top__link">Scopri</a>
+        </div>
+        <img src="<?= get_template_directory_uri(); ?>/images/mumac-top-img.jpg" alt="" class="page-top__img">
     </div>
 
     <div class="mumac-main">
         <div class="col-md-3">
-            <h3 class="mumac-main__header"></h3>
-            <div class="mumac-main__text"></div>
-            <div class="mumac-main__link"></div>
-            <div class="mumac-main__arrows"></div>
+            <h3 class="mumac-main__header">
+                Lorem 
+                ipsum amor
+                ipsum amor amor
+            </h3>
+            <p class="mumac-main__text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do. eiusmod tempor incididunt ut labore et dolore.
+                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...
+            </p>
+            <a href="#" class="mumac-main__link">
+                Leggi di più
+            </a>
+            <div class="mumac-main__arrows">
+                <a href="" class="mumac-main__arrow mumac-main__arrow__left"></a>
+                <a href="" class="mumac-main__arrow mumac-main__arrow__right"></a>
+            </div>
         </div>
         <div class="col-md-8 col-md-offset-1">
-            <img src="" alt="" class="mumac-main__img">
+            <img src="<?= get_template_directory_uri(); ?>/images/mumac-logo-big.png" alt="" class="mumac-main__img">
         </div>
     </div>
 
     <div class="mumac-eventi">
-        <h2 class="mumac-eventi__header"></h2>
+        <h2 class="mumac-eventi__header">
+            Eventi
+        </h2>
         <div class="mumac-eventi__events-block">
             <div class="mumac-eventi__event">
-                <img src="" alt="" class="mumac-eventi__event__img">
-                <h3 class="mumac-eventi__event__header"></h3>
-                <p class="mumac-eventi__event__description"></p>
+                <img src="<?= get_template_directory_uri(); ?>/images/mumac-logo-big.png" alt="" class="mumac-eventi__event__img">
+                <h3 class="mumac-eventi__event__header">
+                    Evento MUMAC
+                </h3>
+                <p class="mumac-eventi__event__description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do. eiusm od tempor incididunt ut labore et dolore.
+                </p>
             </div>
         </div>
     </div>
 
 
     <div class="stay-tuned__block">
-        <h2 class="stay-tuned__header"></h2>
+        <h2 class="stay-tuned__header">
+            Stay tuned
+        </h2>
         <form action="">
             <input type="email" class="stay-tuned__email">
             <input type="checkbox" class="stay-tuned__checkbox">
-            <button class="stay-tuned__button"></button>
+            <a href="" class="stay-tuned__policy"></a>
+            <button type="submit" class="stay-tuned__button">
+                Invia
+            </button>
         </form>
     </div>
 
